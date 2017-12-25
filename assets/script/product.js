97a5a71f81ce4e9692978a2d702d297b0219acd9
naij <naijser@gmail.com>
2017-12-25 19:43:47 +0800

    up

---
+++ assets/script/product.js
@@ -0,0 +1,22 @@
+var PRODUCT = [
+  {
+    timestamp: '20171225140101',
+    title: '毛竹片',
+    desc: '毛竹片'
+  },
+  {
+    timestamp: '20171225140201',
+    title: '毛竹片',
+    desc: '毛竹片'
+  },
+  {
+    timestamp: '20171225140202',
+    title: '毛竹片',
+    desc: '毛竹片'
+  },
+  {
+    timestamp: '20171225140301',
+    title: '毛竹片',
+    desc: '毛竹片'
+  }
+]
