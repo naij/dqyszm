ee1b5d72f33a22f8d0a170218571f5b648dd9104
naij <naijser@gmail.com>
2018-01-07 15:48:13 +0800

    up

--- assets/script/product.js
+++ assets/script/product.js
@@ -1,36 +1,36 @@
 var PRODUCT = [
   {
-    timestamp: '20180106150001',
+    id: 'cc82f8b6aba545d8721ca38d01f13031',
     title: '毛竹',
     desc: '毛竹'
   },
   {
-    timestamp: '20180106150901',
+    id: '3b913cf81226e289d87ecfcc471869c7',
     title: '毛竹',
     desc: '毛竹'
   },
   {
-    timestamp: '20180106151201',
+    id: 'f6e2a3b43c99e982e76e4977516cce37',
     title: '小竹',
     desc: '小竹'
   },
   {
-    timestamp: '20180106151401',
+    id: '4fcec3e1408ab172a9bf28a6cd4b3f5a',
     title: '毛竹',
     desc: '毛竹'
   },
   {
-    timestamp: '20180106151601',
+    id: '7fca569fefb089795ae9bbc662b80edc',
     title: '脚手片',
     desc: '脚手片'
   },
   {
-    timestamp: '20180106153901',
+    id: '20f4863a3d5b62c8ba82edb74ac81775',
     title: '脚手片',
     desc: '脚手片'
   },
   {
-    timestamp: '20180106154101',
+    id: 'edd9bdd8f9f7f13af3c6f2ba4661ffba',
     title: '毛竹',
     desc: '毛竹'
   }
